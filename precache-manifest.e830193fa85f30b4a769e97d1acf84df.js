624b7903e36e0c0cd583fdb91ec252e0ad13befa
AungPhoneKhant <akha0030@student.monash.edu>
2020-04-16 17:21:55 +1000

    Updates

---
+++ precache-manifest.e830193fa85f30b4a769e97d1acf84df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1887e8276d76ccfbe5853be2a0c3a2c9",
+    "url": "/mellowdy/index.html"
+  },
+  {
+    "revision": "e209eceb26a2623afe01",
+    "url": "/mellowdy/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2b8a89d8f9e4329401f3",
+    "url": "/mellowdy/static/css/main.1ab6e363.chunk.css"
+  },
+  {
+    "revision": "e209eceb26a2623afe01",
+    "url": "/mellowdy/static/js/2.3bdf0325.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/mellowdy/static/js/2.3bdf0325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8a89d8f9e4329401f3",
+    "url": "/mellowdy/static/js/main.47df3108.chunk.js"
+  },
+  {
+    "revision": "8accdf0482cb9d993422",
+    "url": "/mellowdy/static/js/runtime-main.87a71600.js"
+  }
+]);
